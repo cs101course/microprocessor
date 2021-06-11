bc4a7fc787e25f6f0658f26e435252c26f6a71f7
David Collien <david@openlearning.com>
2021-06-11 20:02:02 +1000

    Package Fixes

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { assemble as export_assemble } from './assembler/assembler';
+import { Processor as export_Processor} from './processor';
+
+export const assemble = export_assemble;
+export const Processor = export_Processor;
