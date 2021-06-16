ddf3da633f1edbd84b4eb73c06be8ed8eec70f4a
David Collien <david@openlearning.com>
2021-06-16 11:50:56 +1000

    export processorstate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { assemble as export_assemble } from './assembler/assembler';
 import { Processor as export_Processor} from './processor';
+import { ProcessorState as export_ProcessorState} from './state';
 
 export const assemble = export_assemble;
 export const Processor = export_Processor;
+export const ProcessorState = export_ProcessorState;
